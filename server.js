5ed292aee9a5dabb069ca4fd20cf10d88a2ea181
iamakay18 <iamakay18@gmail.com>
2017-08-14 10:14:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', "THIS IS A ARTICLE PAGE");
+app.get('/article-one', function(req, res){
+    "This is Article one";
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
