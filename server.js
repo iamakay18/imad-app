27a2db02b02fd638b28182c4c45e46a425645886
iamakay18 <iamakay18@gmail.com>
2017-08-19 12:46:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,12 @@ app.get('/article-three', function(req, res){
     res.send("This is article three");
 });
 
+var counter = 0;
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
