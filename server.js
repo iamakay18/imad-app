573d227da77d30ad0d6b354e501480971e59d37b
iamakay18 <iamakay18@gmail.com>
2017-09-01 21:39:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/login', function(req, res){
     var username = req.body.username; 
     var password = req.body.password;
        
-    pool.query('SELECT * FROM "users" WHERE usename = $1', [username], function(err, result){
+    pool.query('SELECT * FROM "users" WHERE username = $1', [username], function(err, result){
       if(err){
           res.status(500).send(err.toString());
         } else{
