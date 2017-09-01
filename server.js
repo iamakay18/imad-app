334f0ac2452c8de9ef528a84a6aaf20b39631e4a
iamakay18 <iamakay18@gmail.com>
2017-09-01 20:38:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ function hash(input, salt){
 
 app.get('/hash/:input', function(req, res){
     
-    var hashedString = hash(req.params.input, 'this-is-some-random-salt');
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
 
