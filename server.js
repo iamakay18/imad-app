e320256d1265496b96dbe90d89a0834cea41d803
iamakay18 <iamakay18@gmail.com>
2017-09-01 20:37:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/test-db', function(req, res){
 
 function hash(input, salt){
     var hashedString = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashedString;
+    return hashedString.toString('hex');
 }
 
 app.get('/hash/:input', function(req, res){
